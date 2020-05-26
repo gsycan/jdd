b16f3ada3374f99a5922cb40bb34a87e8b0b6585
guosiyu8 <guosiyu8@jd.com>
2020-05-26 16:13:55 +0800

    from IDEA to Git v1

--- src/main/java/com/jdd/controller/helloGitTest.java
+++ src/main/java/com/jdd/controller/helloGitTest.java
@@ -3,5 +3,6 @@ package com.jdd.controller;
 public class helloGitTest {
     public static void main(String[] args) {
         System.out.println("HellO Git! i come from IDEA");
+        System.out.println("HellO Git! i come from IDEA---commit-2");
     }
 }
